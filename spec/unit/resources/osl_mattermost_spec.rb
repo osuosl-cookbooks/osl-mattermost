e9d26944d79567e74eb3b3db5a52d500234ca831
Lance Albertson <lance@osuosl.org>
2023-07-24 17:20:11 -0700

    Create initial osl_mattermost resource
    Signed-off-by: Lance Albertson <lance@osuosl.org>

---
+++ spec/unit/resources/osl_mattermost_spec.rb
@@ -0,0 +1,115 @@
+require_relative '../../spec_helper'
+
+describe 'mattermost_test::default' do
+  platform 'almalinux', '8'
+  cached(:subject) { chef_run }
+  step_into :osl_mattermost
+
+  before do
+    stub_data_bag_item('nginx', 'dhparam').and_return(
+      'id' => 'dhparam',
+      'key' => 'dh param key'
+    )
+  end
+
+  %w(
+    osl-acme
+    osl-docker
+    osl-git
+    osl-nginx
+  ).each do |r|
+    it { is_expected.to include_recipe r }
+  end
+
+  it do
+    is_expected.to create_acme_selfsigned('mm.example.org').with(
+      crt: '/etc/pki/tls/mattermost.crt',
+      key: '/etc/pki/tls/mattermost.key'
+    )
+  end
+
+  it { expect(chef_run.acme_selfsigned('mm.example.org')).to notify('nginx_service[osuosl]').to(:reload) }
+
+  it do
+    is_expected.to create_cookbook_file('/etc/nginx/conf.d/mattermost.conf').with(
+      source: 'nginx.conf.erb',
+      cookbook: 'osl-mattermost'
+    )
+  end
+
+  it { expect(chef_run.cookbook_file('/etc/nginx/conf.d/mattermost.conf')).to notify('nginx_service[osuosl]').to(:reload) }
+  it { is_expected.to create_directory('/var/www/acme').with(recursive: true) }
+
+  it do
+    is_expected.to create_acme_certificate('mm.example.org').with(
+      crt: '/etc/pki/tls/mattermost.crt',
+      key: '/etc/pki/tls/mattermost.key',
+      wwwroot: '/var/www/acme'
+    )
+  end
+
+  it { expect(chef_run.acme_certificate('mm.example.org')).to notify('nginx_service[osuosl]').to(:reload) }
+  it { is_expected.to install_package 'tar' }
+
+  it do
+    is_expected.to install_ark('mmctl').with(
+      url: 'https://github.com/mattermost/mmctl/releases/download/v7.10.4/linux_amd64.tar',
+      prefix_root: '/opt',
+      prefix_home: '/opt',
+      strip_components: 0,
+      version: '7.10.4'
+    )
+  end
+
+  it { expect(chef_run.link('/usr/local/bin/mmctl')).to link_to('/opt/mmctl/mmctl') }
+
+  it do
+    is_expected.to sync_git('/var/lib/mattermost').with(
+      repository: 'https://github.com/mattermost/docker'
+    )
+  end
+
+  it { expect(chef_run.git('/var/lib/mattermost')).to notify('osl_dockercompose[mattermost]').to(:rebuild) }
+
+  it do
+    is_expected.to create_directory('/var/lib/mattermost/volumes/app/mattermost').with(
+      owner: 2000,
+      group: 2000,
+      recursive: true
+    )
+  end
+
+  %w(
+    bleve-indexes
+    client
+    client/plugins
+    config
+    data
+    logs
+    plugins
+  ).each do |d|
+    it do
+      is_expected.to create_directory("/var/lib/mattermost/volumes/app/mattermost/#{d}").with(
+        owner: 2000,
+        group: 2000,
+        recursive: true
+      )
+    end
+  end
+
+  it do
+    is_expected.to create_template('/var/lib/mattermost/.env').with(
+      source: 'env.erb',
+      cookbook: 'osl-mattermost',
+      variables: {
+        edition: 'team',
+        domain: 'mm.example.org',
+        timezone: 'UTC',
+        version: '7.8',
+      }
+    )
+  end
+
+  it { expect(chef_run.template('/var/lib/mattermost/.env')).to notify('osl_dockercompose[mattermost]').to(:rebuild) }
+  it { expect(chef_run.template('/var/lib/mattermost/.env')).to notify('osl_dockercompose[mattermost]').to(:restart) }
+end
