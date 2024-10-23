1e59cd774fc01180d30ebba9cf39e228279f8c4b
ArtofBugs <74070945+ArtofBugs@users.noreply.github.com>
2024-10-23 12:22:18 -0700

    Fix cookstyle - surround regex with %r{}

--- test/integration/default/controls/default.rb
+++ test/integration/default/controls/default.rb
@@ -119,7 +119,7 @@ control 'default' do
     its('exit_status') { should eq 0 }
     its('stdout') { should eq '' }
     # This comes from upstream files not managed by Chef.
-    its('stderr') { should match /^time="[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z" level=warning msg="\/var\/lib\/mattermost\/docker-compose\.yml: the attribute `version` is obsolete, it will be ignored, please remove it to avoid potential confusion"\n/ }
+    its('stderr') { should match %r{^time="[0-9]+-[0-9]+-[0-9]+T[0-9]+:[0-9]+:[0-9]+Z" level=warning msg="/var/lib/mattermost/docker-compose\.yml: the attribute `version` is obsolete, it will be ignored, please remove it to avoid potential confusion"\n} }
   end
 
   describe cron 'root' do
